05e3a0abba5eb01b44cb2a130d876a8bcbec0a17
luolihao@corp.netease.com <luolihao@corp.netease.com>
2022-08-25 14:41:00 +0800

    commit v1.8

--- Classes/NTESBaseComponent.framework/Headers/NTESBasePublicData.h
+++ Classes/NTESBaseComponent.framework/Headers/NTESBasePublicData.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#define NTESBASEESDKVERSION @"1.7"
+#define NTESBASEESDKVERSION @"1.8"
 
 typedef NS_ENUM(NSUInteger, NTESSendErrorEnvironment) {
     NTESSendErrorEnvironmentTest, // 测试环境
